df1e9891a9bb73203aeb3dc665ac6364bcd0cf2c
Faith-vaati <vaati.faith79@gmail.com>
2024-10-14 14:43:27 +0300

    setting up APP_WIDTH and APP_HEIGHT as a percentage of the screen size

--- run.py
+++ run.py
@@ -11,16 +11,24 @@ from utils.constants import APP_WIDTH, APP_HEIGHT
 # Create and run the Tkinter application
 if __name__ == "__main__":
     root = tk.Tk()
+
+    # Get the screen width and height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # Set APP_WIDTH and APP_HEIGHT as a percentage of the screen size
+    APP_WIDTH = int(80 * screen_width / 100)  # 80% of screen width
+    APP_HEIGHT = int(80 * screen_height / 100)  # 80% of screen height
+
     root.geometry(f'{APP_WIDTH}x{APP_HEIGHT}')
     root.title("Conway's Game of Life")
 
-    # initialize the game of life logic and canvas for drawing the grid
+    # Initialize the game of life logic and canvas for drawing the grid
     game_of_life = GameOfLife()
     game_canvas = GameCanvas(root, game_of_life)
 
-    # initialize the game and run it
+    # Initialize the game and run it
     game = Game(root, game_canvas, game_of_life)
-    game.run()
 
-    # start the main loop
+    game.run()
     root.mainloop()
